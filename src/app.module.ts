8357bad5250948ccfdeb92ffe13c3dc72187c9fc
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-07-28 19:39:12 +0900

    feat : users module/controller/service 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     AuthModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    UsersModule,
   ],
   controllers: [],
   providers: [],
