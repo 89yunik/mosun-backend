4b51c9cc90a3bd2cc039d8f616adf51386f80aeb
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-07-28 17:31:22 +0900

    chore : app.controller, service 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ApiModule } from './api/api.module';
+import { AuthModule } from './auth/auth.module';
 @Module({
-  imports: [ApiModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getMain(): string {
-    return 'main page';
-  }
-}
