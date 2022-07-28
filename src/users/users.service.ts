f695efdc882936e94e9f24ab9e64268c9064eec7
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-07-28 20:59:55 +0900

    feat : user service -  email 로그인 기능 추가

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
 
+// This should be a real class/interface representing a user entity
+export type User = any;
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users = [
+    {
+      email: 'changeme@test.com',
+      name: 'john',
+    },
+    {
+      email: 'guess@test.com',
+      name: 'maria',
+    },
+  ];
+
+  async findOrCreateUser(
+    email: string,
+    name: string,
+  ): Promise<User | undefined> {
+    const user = this.users.find((user) => user.name === name);
+    if (user) {
+      return user;
+    } else {
+      this.users.push({ email, name });
+      return this.users[this.users.length - 1];
+    }
+  }
+}
