d0eeb503c0d5142e834cf56abf37bcaf8ef54814
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-07-31 23:49:00 +0900

    fix : 구글 로그인 bug 수정 - scope 추가, throw error 제거

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -15,16 +15,13 @@ export class UsersService {
       name: 'maria',
     },
   ];
-  async createUser(user: User): Promise<User> {
-    this.users.push(user);
-    return this.users[this.users.length - 1];
+  async createUser(target: User): Promise<User> {
+    this.users.push(target);
+    const user = this.users[this.users.length - 1];
+    return user;
   }
   async readUserByEmail(target: User): Promise<User | undefined> {
     const user = this.users.find((user) => user.email === target.email);
-    if (user) {
-      return user;
-    } else {
-      throw new Error('입력한 값에 해당하는 사용자를 찾을 수 없습니다.');
-    }
+    return user;
   }
 }
