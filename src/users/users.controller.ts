3e513e98da19a5ab87c9037d008551565bd1e279
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-08-01 23:17:39 +0900

    feat : profile, logout 기능 구현

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Res, UseGuards } from '@nestjs/common';
+import { Response } from 'express';
+import { User } from 'src/auth/decorators/user.decorator';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { UsersService, UserType } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+  @Get('profile')
+  @UseGuards(JwtAuthGuard)
+  getProfile(@User() user: UserType) {
+    return user;
+  }
+
+  @Get('logout')
+  @UseGuards(JwtAuthGuard)
+  logout(@Res() res: Response) {
+    res.clearCookie('accessToken');
+    res.clearCookie('refreshToken').redirect(process.env.AUTH_REDIRECT);
+  }
+}
