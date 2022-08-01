55fdf85d07e7d5ddb536ae342af0316651a91939
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-08-01 23:17:15 +0900

    feat : kakao/google 로그인 기능 구현 - jwt token, cookie 사용

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,10 @@
-import {
-  Controller,
-  Req,
-  Post,
-  Get,
-  UseGuards,
-  HttpCode,
-} from '@nestjs/common';
+import { Controller, Get, UseGuards, HttpCode, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { KakaoAuthGuard } from './guards/kakao-auth.guard';
 import { GoogleAuthGuard } from './guards/google-auth.guard';
-import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { User } from './decorators/user.decorator';
 import { UserType } from 'src/users/users.service';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -24,18 +17,44 @@ export class AuthController {
   @Get('google/callback')
   @UseGuards(GoogleAuthGuard)
   @HttpCode(200)
-  googleCallback(@User() user: UserType): void {
-    console.log(this.authService.setToken(user));
-    return;
+  googleCallback(@User() user: UserType, @Res() res: Response): void {
+    const accessExp = Number(process.env.JWT_ACCESS_EXP);
+    const refreshExp = Number(process.env.JWT_REFRESH_EXP);
+    const accessToken = this.authService.setToken('access', user);
+    const refreshToken = this.authService.setToken('refresh');
+    res.cookie('accessToken', accessToken, {
+      maxAge: accessExp * 60 * 1000,
+      httpOnly: true,
+    });
+    res
+      .cookie('refreshToken', refreshToken, {
+        maxAge: refreshExp * 60 * 60 * 1000,
+        httpOnly: true,
+      })
+      .redirect(process.env.AUTH_REDIRECT);
   }
 
   @Get('kakao')
   @UseGuards(KakaoAuthGuard)
   kakaoLogin(): void {}
 
-  @UseGuards(JwtAuthGuard)
-  @Get('profile')
-  getProfile(@User() user: UserType) {
-    return user;
+  @Get('kakao/callback')
+  @UseGuards(KakaoAuthGuard)
+  @HttpCode(200)
+  kakaoCallback(@User() user: UserType, @Res() res: Response): void {
+    const accessExp = Number(process.env.JWT_ACCESS_EXP);
+    const refreshExp = Number(process.env.JWT_REFRESH_EXP);
+    const accessToken = this.authService.setToken('access', user);
+    const refreshToken = this.authService.setToken('refresh');
+    res.cookie('accessToken', accessToken, {
+      maxAge: accessExp * 60 * 1000,
+      httpOnly: true,
+    });
+    res
+      .cookie('refreshToken', refreshToken, {
+        maxAge: refreshExp * 60 * 60 * 1000,
+        httpOnly: true,
+      })
+      .redirect(process.env.AUTH_REDIRECT);
   }
 }
