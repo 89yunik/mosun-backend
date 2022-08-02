0d7200ab9feb41f6c71ffdf7e11cb35bae112aa8
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-08-02 22:49:57 +0900

    setting : 테스트 코드 작성

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,5 +1,8 @@
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { UsersService } from 'src/users/users.service';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -7,6 +10,7 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [AuthService, UsersService, JwtService],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
@@ -15,4 +19,6 @@ describe('AuthController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('should return a user', () => {});
 });
