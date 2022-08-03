91e8556706063cce9e81d72f672e7e259e315086
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-08-03 23:50:57 +0900

    refactor : readUserByEmail 매개변수 형식 수정

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -19,11 +19,14 @@ export class UsersService {
     },
   ];
   async createUser(target: UserType): Promise<UserType> {
+    //email 형식이 아니면 저장하지 않는 로직 필요
     this.users.push(target);
     const user = this.users[this.users.length - 1];
     return user;
   }
-  async readUserByEmail(target: UserType): Promise<UserType | undefined> {
+  async readUserByEmail(
+    target: Partial<UserType>,
+  ): Promise<UserType | undefined> {
     const user = this.users.find((user) => user.email === target.email);
     return user;
   }
