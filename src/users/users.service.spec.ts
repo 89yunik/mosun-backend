5bd273f5df6d6519259b716d268b39dfeac8d6cb
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-08-03 23:50:11 +0900

    refactor : UsersService 테스트 코드 작성

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -15,4 +15,18 @@ describe('UsersService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should return a created user', async () => {
+    const target = { email: 'test', name: 'test' };
+    expect(await service.createUser(target)).toEqual(target);
+  });
+
+  it('should return a user with that email', async () => {
+    const target = { email: 'changeme@test.com' };
+    const result = {
+      email: 'changeme@test.com',
+      name: 'john',
+    };
+    expect(await service.readUserByEmail(target)).toEqual(result);
+  });
 });
