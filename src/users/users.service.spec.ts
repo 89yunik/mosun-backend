0fd7499c2cdbabc91abce0afac372bba051fbc65
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-08-04 17:29:00 +0900

    fix : UsersService 수정 - email 형식 체크 및 테스트코드 작성

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -14,19 +14,46 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+    expect(service.createUser).toBeDefined();
+    expect(service.readUserByEmail).toBeDefined();
   });
 
   it('should return a created user', async () => {
-    const target = { email: 'test', name: 'test' };
-    expect(await service.createUser(target)).toEqual(target);
+    const validEmail = { email: 'test@test.com', name: 'test' };
+    expect(await service.createUser(validEmail)).toEqual(validEmail);
+
+    const invalidEmail = { email: '@test.com', name: 'test' };
+    const expectedError = new Error(
+      '생성하려는 이메일 형식이 유효하지 않습니다.',
+    );
+    expectedError.name = 'Unprocessable Entity';
+    expect(
+      await service.createUser(invalidEmail).catch((error) => {
+        expect(error).toEqual(expectedError);
+      }),
+    ).toEqual(undefined);
   });
 
   it('should return a user with that email', async () => {
-    const target = { email: 'changeme@test.com' };
-    const result = {
+    const existentEmail = 'changeme@test.com';
+    const result1 = {
       email: 'changeme@test.com',
       name: 'john',
     };
-    expect(await service.readUserByEmail(target)).toEqual(result);
+    expect(await service.readUserByEmail(existentEmail)).toEqual(result1);
+
+    const nonExistentEmail = 'test@test.com';
+    expect(await service.readUserByEmail(nonExistentEmail)).toEqual(undefined);
+
+    const invalidEmail = '@test.com';
+    const expectedError = new Error(
+      '찾으려는 이메일 형식이 유효하지 않습니다.',
+    );
+    expectedError.name = 'Unprocessable Entity';
+    expect(
+      await service.readUserByEmail(invalidEmail).catch((error) => {
+        expect(error).toEqual(expectedError);
+      }),
+    ).toEqual(undefined);
   });
 });
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
 
-// This should be a real class/interface representing a user entity
 export type UserType = {
-  email: string;
+  email: string; //Email type?
   name: string;
+};
+
+const validateEmail = (email: string): boolean => {
+  const emailPattern = /\w+@\w+.+\w/;
+  return email.match(emailPattern) ? true : false;
 };
 
 @Injectable()
@@ -19,15 +23,24 @@ export class UsersService {
     },
   ];
   async createUser(target: UserType): Promise<UserType> {
-    //email 형식이 아니면 저장하지 않는 로직 필요
-    this.users.push(target);
-    const user = this.users[this.users.length - 1];
-    return user;
+    if (validateEmail(target.email)) {
+      this.users.push(target);
+      const user = this.users[this.users.length - 1];
+      return user;
+    } else {
+      const error = new Error('생성하려는 이메일 형식이 유효하지 않습니다.');
+      error.name = 'Unprocessable Entity';
+      throw error;
+    }
   }
-  async readUserByEmail(
-    target: Partial<UserType>,
-  ): Promise<UserType | undefined> {
-    const user = this.users.find((user) => user.email === target.email);
-    return user;
+  async readUserByEmail(email: string): Promise<UserType | undefined> {
+    if (validateEmail(email)) {
+      const user = this.users.find((user) => user.email === email);
+      return user;
+    } else {
+      const error = new Error('찾으려는 이메일 형식이 유효하지 않습니다.');
+      error.name = 'Unprocessable Entity';
+      throw error;
+    }
   }
 }
