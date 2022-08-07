4bceb12a22481f363719e7282386d4acfb4ddb9c
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-08-07 23:46:58 +0900

    test : auth service 테스트

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -16,5 +16,21 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+    expect(service.setToken).toBeDefined();
+  });
+
+  it('should return a jwt token', () => {
+    const loginedUser = { email: 'loginedUser@jwt.com', name: 'loginedUser' };
+    let jwtService = new JwtService({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: `${process.env.JWT_BASIC_EXP}m` },
+    });
+
+    expect(
+      jwtService.verify(service.setToken('access', loginedUser)),
+    ).toMatchObject(loginedUser);
+    expect(
+      jwtService.verify(service.setToken('refresh', loginedUser)),
+    ).toBeDefined();
   });
 });
