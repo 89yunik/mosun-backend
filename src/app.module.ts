8a6ec40d9209c561453e4230812ee603290f1e9b
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-08-08 13:18:02 +0900

    setting : mysql db 연동을 위한 package 설치,  app.module에 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
@@ -10,6 +11,16 @@ import { UsersModule } from './users/users.module';
       isGlobal: true,
     }),
     UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.TYPEORM_HOST,
+      port: Number(process.env.TYPEORM_PORT),
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      entities: ['entity/*.ts'],
+      synchronize: true,
+    }),
   ],
   controllers: [],
   providers: [],
