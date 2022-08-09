3467f972c98c7e88081dd2a3bce65b22a3ba44be
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-08-09 17:01:28 +0900

    feat : updateUser 추가 - error 처리 nest.js 방식으로 조정, readUserByEmail(email) -> readUser(options)으로 수정

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, UpdateResult } from 'typeorm';
 import { User } from './user.entity';
 
 const validateEmail = (email: string): boolean => {
@@ -21,38 +25,64 @@ export class UsersService {
         await this.usersRepository.save(user);
         return { email: user.email, name: user.name };
       } else {
-        const error = new Error('');
-        error.name = '';
-        throw error;
+        throw new BadRequestException(
+          '생성하려는 계정의 필수 입력값을 확인해 주십시오.',
+        );
       }
     } else {
-      const error = new Error('생성하려는 이메일 형식이 유효하지 않습니다.');
-      error.name = 'Unprocessable Entity';
-      throw error;
+      throw new UnprocessableEntityException(
+        '생성하려는 이메일 형식이 유효하지 않습니다.',
+      );
     }
   }
   async readUsers(name?: string) {
     return this.usersRepository.find();
   }
-  async readUserByEmail(email: string): Promise<Partial<User> | undefined> {
-    if (validateEmail(email)) {
-      const user = await this.usersRepository.findOneBy({ email });
+
+  async readUser(options: Partial<User>): Promise<Partial<User> | undefined> {
+    if (options.email) {
+      if (validateEmail(options.email)) {
+        const user = await this.usersRepository.findOneBy(options);
+        if (user) {
+          return { email: user.email, name: user.name };
+        }
+      } else {
+        throw new UnprocessableEntityException(
+          '찾으려는 이메일 형식이 유효하지 않습니다.',
+        );
+      }
+    } else {
+      const user = await this.usersRepository.findOneBy(options);
       if (user) {
         return { email: user.email, name: user.name };
       }
-    } else {
-      const error = new Error('찾으려는 이메일 형식이 유효하지 않습니다.');
-      error.name = 'Unprocessable Entity';
-      throw error;
     }
   }
+  // async readUserByEmail(email: string): Promise<Partial<User> | undefined> {
+  //   if (validateEmail(email)) {
+  //     const user = await this.usersRepository.findOneBy({ email });
+  //     if (user) {
+  //       return { email: user.email, name: user.name };
+  //     }
+  //   } else {
+  //     throw new UnprocessableEntityException(
+  //       '찾으려는 이메일 형식이 유효하지 않습니다.',
+  //     );
+  //   }
+  // }
 
   async readOrCreateUser(target: Partial<User>): Promise<Partial<User>> {
-    let user = await this.readUserByEmail(target.email);
+    let user = await this.readUser(target);
     if (!user) {
       console.log('회원가입');
       user = await this.createUser(target);
     }
     return user;
+  }
+  async updateUser(
+    target: Partial<User>,
+    update: Partial<User>,
+  ): Promise<void> {
+    await this.usersRepository.update(target, update);
   }
 }
