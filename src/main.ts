a20559a155f0cd87516a80b376af382b25122707
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-08-11 15:29:52 +0900

    setting : class-validator 세팅

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
   app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
