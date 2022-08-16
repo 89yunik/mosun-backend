96b9fa158e72e6ec1080f91b93f131a47d99a28f
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-08-16 13:32:25 +0900

    feat : groups 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { GroupsModule } from './groups/groups.module';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { AuthModule } from './auth/auth.module';
     }),
     UsersModule,
     AuthModule,
+    GroupsModule,
   ],
   controllers: [],
   providers: [],
