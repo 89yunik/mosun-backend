58b088d1eb85edbb3b952306ea8cad326d733075
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-08-18 16:13:26 +0900

    setting : teams.module 세팅 - MembersRepository import, MembersService provide

--- src/teams/teams.module.ts
+++ src/teams/teams.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Member } from 'src/members/member.entity';
+import { MembersService } from 'src/members/members.service';
 import { Team } from './team.entity';
 import { TeamsController } from './teams.controller';
 import { TeamsService } from './teams.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Team])],
+  imports: [
+    TypeOrmModule.forFeature([Team]),
+    TypeOrmModule.forFeature([Member]),
+  ],
   controllers: [TeamsController],
-  providers: [TeamsService],
+  providers: [TeamsService, MembersService],
 })
 export class TeamsModule {}
