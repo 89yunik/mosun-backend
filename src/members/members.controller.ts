9bd8ae525772f8d76d582c4abae65230a72767b7
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-08-21 23:16:33 +0900

    feat : members.controller CRUD - swagger 동작 확인

--- src/members/members.controller.ts
+++ src/members/members.controller.ts
@@ -1,5 +1,22 @@
-import { Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
-import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import {
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { Request } from 'express';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { CreateMemberDto } from './dtos/create-member.dto';
@@ -7,34 +24,112 @@ import { Member } from './member.entity';
 import { MembersService } from './members.service';
 
 @ApiTags('Members')
-@Controller('members')
+@Controller('teams/:teamId/members')
 export class MembersController {
   constructor(private membersService: MembersService) {}
   @ApiOperation({
     summary: '팀원 생성 API',
     description: '',
   })
+  @ApiParam({ name: 'userId' })
+  @ApiParam({ name: 'teamId' })
   @ApiBody({ type: CreateMemberDto, required: true })
   @ApiResponse({ status: 200, type: Member })
-  @Post()
+  @Post('users/:userId')
   @UseGuards(JwtAuthGuard)
   createMember(@Req() req: Request): Promise<Member> {
-    const newMemberInfo = req.body;
-    const newMember = this.membersService.createMember(newMemberInfo);
-    return newMember;
+    const teamId = Number(req.params.teamId);
+    const userId = Number(req.params.userId);
+    const memberInfo = req.body;
+    const member = this.membersService.createMember({
+      teamId,
+      userId,
+      ...memberInfo,
+    });
+    return member;
   }
 
   @ApiOperation({
-    summary: '소속팀 조회 API',
-    description: '로그인된 사용자 정보로 소속된 팀을 조회한다.',
+    summary: '팀원 검색 API',
+    description: '검색어와 일치하는 팀원들을 조회한다.',
   })
-  @ApiResponse({ status: 200, type: Member })
-  @Get()
+  @ApiParam({ name: 'teamId' })
+  @ApiResponse({ status: 200, type: Member, isArray: true })
+  @Get('users')
   @UseGuards(JwtAuthGuard)
-  async readMembersOfUser(@Req() req: Request): Promise<Member[]> {
-    const members = await this.membersService.readMembers({
-      userId: req.user.id,
+  readMembers(@Req() req: Request): Promise<Member[]> {
+    const teamId = Number(req.params.teamId);
+    const memberInfo = req.body;
+    return this.membersService.readMembers({ teamId, ...memberInfo });
+  }
+
+  @ApiOperation({
+    summary: '팀원 수정 API',
+    description: '사용자가 관리하는 팀의 팀원 권한을 수정한다.',
+  })
+  @ApiParam({ name: 'memberId' })
+  @ApiParam({ name: 'teamId' })
+  @ApiBody({ type: CreateMemberDto })
+  @ApiResponse({ status: 200 })
+  @Put(':memberId')
+  @UseGuards(JwtAuthGuard)
+  async updateMember(@Req() req) {
+    const teamId = Number(req.params.teamId);
+    const userId = req.user.id;
+    const admin = await this.membersService.readMember({
+      userId,
+      teamId,
     });
-    return members;
+    if (admin) {
+      if (admin.authority === 'admin') {
+        const memberId = Number(req.params.memberId);
+        const update = req.body;
+        await this.membersService.updateMember({ id: memberId }, update);
+      } else {
+        throw new HttpException(
+          '팀원 수정 권한이 없습니다.',
+          HttpStatus.FORBIDDEN,
+        );
+      }
+    } else {
+      throw new HttpException(
+        '수정하려는 팀의 팀원이 아닙니다.',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  @ApiOperation({
+    summary: '팀원 삭제 API',
+    description: '사용자가 관리하는 팀의 팀원을 삭제한다.',
+  })
+  @ApiParam({ name: 'memberId' })
+  @ApiParam({ name: 'teamId' })
+  @ApiResponse({ status: 200 })
+  @Delete(':memberId')
+  @UseGuards(JwtAuthGuard)
+  async deleteMember(@Req() req) {
+    const userId = req.user.id;
+    const teamId = req.params.teamId;
+    const admin = await this.membersService.readMember({
+      userId,
+      teamId,
+    });
+    if (admin) {
+      if (admin.authority === 'admin') {
+        const memberId = Number(req.params.memberId);
+        this.membersService.deleteMember({ id: memberId });
+      } else {
+        throw new HttpException(
+          '팀원 삭제 권한이 없습니다.',
+          HttpStatus.FORBIDDEN,
+        );
+      }
+    } else {
+      throw new HttpException(
+        '삭제하려는 팀의 팀원이 아닙니다.',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 }
