c1f2c757e2f19c3fdb13aa9cd5bc48fc9eb0889e
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-08-21 23:16:59 +0900

    fix : swagger 관련 body type 수정

--- src/members/dtos/create-member.dto.ts
+++ src/members/dtos/create-member.dto.ts
@@ -2,11 +2,9 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateMemberDto {
-  @ApiProperty()
   @IsNumber()
   @IsNotEmpty()
   teamId: number;
-  @ApiProperty()
   @IsNumber()
   @IsNotEmpty()
   userId: number;
