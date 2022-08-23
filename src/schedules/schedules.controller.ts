20143cf7466b6ad53b93e16867b0d89aa3363890
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-08-23 16:31:10 +0900

    feat : schedules.service/controller 작성 - 기본 CRUD, 다른 모듈 참조

--- src/schedules/schedules.controller.ts
+++ src/schedules/schedules.controller.ts
@@ -1,4 +1,73 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Post, Put, Req, UseGuards } from '@nestjs/common';
+import {
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { Request } from 'express';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { CreateScheduleDto } from './dtos/create-schedule.dto';
+import { UpdateScheduleDto } from './dtos/update-schedule.dto';
+import { SchedulesService } from './schedules.service';
 
+@ApiTags('Schedules')
 @Controller('schedules')
-export class SchedulesController {}
+export class SchedulesController {
+  constructor(private schedulesService: SchedulesService) {}
+
+  @ApiOperation({
+    summary: '일정 추가 API',
+    description: '일정을 추가한다.',
+  })
+  @ApiBody({ type: CreateScheduleDto })
+  @ApiResponse({ status: 200 })
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  async createSchedule(@Req() req: Request): Promise<void> {
+    const scheduleInfo = req.body;
+    await this.schedulesService.createSchedule(scheduleInfo);
+  }
+
+  @ApiOperation({
+    summary: '일정 검색 API',
+    description: '검색어와 일치하는 일정들을 조회한다.',
+  })
+  //   @ApiBody({ type: Schedule })
+  @ApiResponse({ status: 200 })
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  async readSchedules(@Req() req: Request): Promise<void> {
+    // const options = req.body;
+    await this.schedulesService.readSchedules();
+  }
+
+  @ApiOperation({
+    summary: '(작성자)일정 수정 API',
+    description: '사용자가 작성한 일정을 수정한다.',
+  })
+  @ApiParam({ name: 'scheduleId' })
+  @ApiBody({ type: UpdateScheduleDto })
+  @ApiResponse({ status: 200 })
+  @Put(':scheduleId')
+  @UseGuards(JwtAuthGuard)
+  async updateSchedule(@Req() req): Promise<void> {
+    const scheduleId = Number(req.params.scheduleId);
+    const update = req.body;
+    await this.schedulesService.updateSchedule({ id: scheduleId }, update);
+  }
+
+  @ApiOperation({
+    summary: '(작성자)일정 삭제 API',
+    description: '사용자가 작성한 일정을 삭제한다.',
+  })
+  @ApiParam({ name: 'scheduleId' })
+  @ApiResponse({ status: 200 })
+  @Delete(':scheduleId')
+  @UseGuards(JwtAuthGuard)
+  async deleteSchedule(@Req() req): Promise<void> {
+    const scheduleId = Number(req.params.scheduleId);
+    this.schedulesService.deleteSchedule({ id: scheduleId });
+  }
+}
--- src/schedules/schedules.service.ts
+++ src/schedules/schedules.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateScheduleDto } from './dtos/create-schedule.dto';
+import { UpdateScheduleDto } from './dtos/update-schedule.dto';
+import { Schedule } from './schedule.entity';
 
 @Injectable()
-export class SchedulesService {}
+export class SchedulesService {
+  constructor(
+    @InjectRepository(Schedule)
+    private schedulesRepository: Repository<Schedule>,
+  ) {}
+
+  async createSchedule(target: CreateScheduleDto): Promise<Schedule> {
+    const schedule = this.schedulesRepository.create(target);
+    await this.schedulesRepository.save(schedule);
+    return schedule;
+  }
+
+  async readSchedules(options?: Partial<Schedule>): Promise<Schedule[]> {
+    return this.schedulesRepository.findBy(options);
+  }
+  async updateSchedule(
+    target: Partial<Schedule>,
+    update: UpdateScheduleDto,
+  ): Promise<void> {
+    await this.schedulesRepository.update(target, update);
+  }
+
+  async deleteSchedule(target: Partial<Schedule>): Promise<void> {
+    await this.schedulesRepository.delete(target);
+  }
+}
