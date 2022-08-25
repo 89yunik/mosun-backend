534f75ebbe8ff40f62aa8608964397fdcbccbb35
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2022-08-25 17:34:40 +0900

    feat : scores.service CRUD

--- src/scores/scores.service.ts
+++ src/scores/scores.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
+import { CreateScoreDto } from './dtos/create-score.dto';
+import { UpdateScoreDto } from './dtos/update-score.dto';
+import { Score } from './score.entity';
 
 @Injectable()
-export class ScoresService {}
+export class ScoresService {
+  constructor(
+    @InjectRepository(Score)
+    private scoresRepository: Repository<Score>,
+  ) {}
+
+  async createScore(target: CreateScoreDto): Promise<Score> {
+    const score = this.scoresRepository.create(target);
+    await this.scoresRepository.save(score);
+    return score;
+  }
+
+  async readScores(options?: Partial<Score>): Promise<Score[]> {
+    return this.scoresRepository.findBy(options);
+  }
+  async updateScore(
+    target: Partial<Score>,
+    update: UpdateScoreDto,
+  ): Promise<UpdateResult> {
+    const updateResult = await this.scoresRepository.update(target, update);
+    return updateResult;
+  }
+
+  async deleteScore(target: Partial<Score>): Promise<DeleteResult> {
+    const deleteResult = await this.scoresRepository.delete(target);
+    return deleteResult;
+  }
+}
